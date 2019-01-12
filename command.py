16cd5a946cb03c6c36fef14cd20b1d7455128aff
Phyungjin <null>
2019-01-12 16:18:26 +0000

    basic command

---
+++ command.py
@@ -0,0 +1,18 @@
+#command.py
+
+#cli // command line interface
+
+#ls // 현재 머물고 있는 디렉토리 내 파일과 디렉토리들 보여주기
+#ls -al  // 현재 머물고 있는 디렉토리의 파일들을 보여줌 (좀더 자세히)
+#ls -l
+
+#pwd // 현재 머물고 있는 디렉토리
+#mkdir // 디렉토리 생성 ex) mkdir hello_linux
+
+#touch // 파일 생성 ex) touch empty_file.txt
+
+#cd // 디렉토리 이동  1. 절대경로 방법 2.상대경로 방법
+#rm // 파일 삭제 (디렉토리 삭제 불가)
+#rm -r // remove directories and their contents recursively
+
+#clear // terminal clear
---
+++ test.py
@@ -0,0 +1,5 @@
+#test.py
+
+print('hello world!');
+
+#run:  python test.py
